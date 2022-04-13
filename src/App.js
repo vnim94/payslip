61ceec59189ac0edfb5a97fb1b101bfb95129ee6
vnim94 <victor.nim94@outlook.com>
2022-04-13 19:23:31 +1000

    implemented payslip calculator, display and file upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import parseCsv from './util/parse';
+import PayslipCalculator from './payslip/payslipCalculator';
+import TaxRateCalulator from './payslip/taxRateCalculator';
+import PayslipCard from './payslip/PayslipCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState();
+    const taxRateCalculator = new TaxRateCalulator();
+    const payslipCalculator = new PayslipCalculator(taxRateCalculator);
+
+    const handleUpload = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader()
+        reader.readAsText(file);
+
+        reader.onload = () => {
+            const data = parseCsv(reader.result.split('\n'));
+            setData(data);
+        }
+
+        reader.onerror = () => {
+            console.log(reader.error)
+        }
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <h1>Payslip Generator</h1>
+                <input type="file" onChange={handleUpload}></input>
+            </div>
+            {data && data.map((entry, index) => {
+                const details = payslipCalculator.calcPayslip(entry);
+                return <PayslipCard key={index} details={details} />
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/payslip/PayslipCard.js
@@ -0,0 +1,32 @@
+import './PayslipCard.css';
+
+function PayslipCard(props) {
+
+    const { name, period, grossIncome, tax, netIncome, superAmount } = props.details
+
+    return (
+        <div className="payslip-card">
+            <h1>{name}</h1>
+            <table>
+                <thead>
+                    <th>Pay Period</th>
+                    <th>Gross Income</th>
+                    <th>Tax</th>
+                    <th>Net Income</th>
+                    <th>Super</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{period}</td>
+                        <td>{grossIncome}</td>
+                        <td>{tax}</td>
+                        <td>{netIncome}</td>
+                        <td>{superAmount}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default PayslipCard;

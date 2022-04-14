a65c2ebe1523888ac312f9298f2f139c4bed38f7
vnim94 <victor.nim94@outlook.com>
2022-04-14 11:06:39 +1000

    updated validator

--- src/util/validator.js
+++ src/util/validator.js
@@ -2,12 +2,13 @@ class Validator {
     validateInput(input) {
         const { firstName, lastName, salary, superRate, period } = input;
         const errors = [];
-        if (firstName.length === 0) errors.push('Empty first name provided');
+        if (firstName.length === 0) errors.push('First name must not be empty');
         if (firstName.match(/[^a-zA-Z]/)) errors.push('First name must only contain letters');
-        if (lastName.length === 0) errors.push('Empty last name provided');
+        if (lastName.length === 0) errors.push('Last name must not be empty');
         if (lastName.match(/[^a-zA-Z]/)) errors.push('Last name must only contain letters');
         if (salary === '0') errors.push('Salary must be greater than 0');
         if (superRate === '0') errors.push('Super rate must be greater than 0');
+        if (period.length === 0) errors.push('Pay period must be provided')
         
         if (errors.length > 0) return errors;
     }
